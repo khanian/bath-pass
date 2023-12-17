0dfc93c6a6beac90ba8c94d9dce067496ca40c1c
khanian <khanylee@gmail.com>
2023-12-17 10:27:48 +0900

    Initial commit

---
+++ src/main/java/com/example/pass/BathPassApplication.java
@@ -0,0 +1,13 @@
+package com.example.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BathPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BathPassApplication.class, args);
+    }
+
+}
